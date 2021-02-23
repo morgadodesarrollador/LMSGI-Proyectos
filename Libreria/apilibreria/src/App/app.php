8ca76de3653f3d8515a3e6411ee8edec02c908f3
Morgado <morgadodesarrollador@gmail.com>
2021-02-23 22:44:12 +0100

    proyectoLibreria 23/02/21

--- Libreria/apilibreria/src/App/app.php
+++ Libreria/apilibreria/src/App/app.php
@@ -2,24 +2,23 @@
 
 use Slim\Factory\AppFactory;
 use Slim\Exception\NotFoundException;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 // cargamos el autoload para que pueda detectar el resto de las clases
 require __DIR__ . '/../../vendor/autoload.php';
 
 
 // creamos la aplicación php
 $app = AppFactory::create();
-$app->get("Libreria/apilibreria/public/hello", function(ServerRequestInterface $request, ResponseInterface $response, $args) {
-	print("Hello world!");
+$app->setBasePath("/Libreria/apilibreria/public/index.php");
+$app->get("/hello", function(Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
     return $response;
 });
 
 //Cargamos en memoria los archivos de rutas que contendrán los entrypoints a cada una de las tablas.
 //los entrypoints harán referencia a las acciones CRUD de una tabla de nuestra BD
-require __DIR__ . "/../Routes/libros.php";
-require __DIR__ . "/../Routes/categorias.php";
-require __DIR__ . "/../Routes/usuarios.php";
+//require __DIR__ . "/../Routes/libros.php";
 
 
 $app->run();
