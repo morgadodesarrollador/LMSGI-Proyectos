d9fc42858a3427507133aeab0b2018299c8c02fe
Morgado <morgadodesarrollador@gmail.com>
2021-02-23 22:28:39 +0100

    proyectoLibreria 23/02/21

--- Libreria/apilibreria/src/App/app.php
+++ Libreria/apilibreria/src/App/app.php
@@ -1,13 +1,18 @@
 <?php
 
 use Slim\Factory\AppFactory;
-#use Slim\Exception\NotFoundException;
+use Slim\Exception\NotFoundException;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 // cargamos el autoload para que pueda detectar el resto de las clases
 require __DIR__ . '/../../vendor/autoload.php';
 
 
 // creamos la aplicación php
 $app = AppFactory::create();
+$app->get("/hello", function(ServerRequestInterface $request, ResponseInterface $response, $args) {
+	print("Hello world!");
+});
 
 //Cargamos en memoria los archivos de rutas que contendrán los entrypoints a cada una de las tablas.
 //los entrypoints harán referencia a las acciones CRUD de una tabla de nuestra BD
