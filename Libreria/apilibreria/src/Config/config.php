16e8d8538615d308507cc2f2f9e4c9318134d365
Morgado <morgadodesarrollador@gmail.com>
2021-02-23 23:14:49 +0100

    proyectoLibreria 23/02/21

--- Libreria/apilibreria/src/Config/config.php
+++ Libreria/apilibreria/src/Config/config.php
@@ -2,7 +2,7 @@
 namespace App\Config;
 
 interface config {
-    CONST DB_NAME    = "libreria";
+    CONST DB_NAME    = "Libreria";
     CONST DB_USER    = "morgado";
     CONST DB_PASS    = "morgado";
     CONST DB_CHARSET = "utf8";
