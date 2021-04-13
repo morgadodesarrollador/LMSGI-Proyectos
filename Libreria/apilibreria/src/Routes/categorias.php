07c6937d1c858895fcc2eaaca1607ba9604d2d1b
Morgado <morgadodesarrollador@gmail.com>
2021-04-13 21:28:48 +0200

    añadir api libreria

--- Libreria/apilibreria/src/Routes/categorias.php
+++
@@ -1,9 +0,0 @@
-<?php
-# para agrupar las rutas en grupos
-use Slim\Routing\RouteCollectorProxy;
-
-//contendrá los entrypoints (acciones CRUD) de la tabla categorias
-$app->group('/api', function(RouteCollectorProxy $group){
-    $group->get('/categorias', 'App\Controllers\CategoriasController:getAll');
-    $group->post('/categorias/new', 'App\Controllers\CategoriasController:new');   
-});
