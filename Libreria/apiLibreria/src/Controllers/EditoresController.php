3737d647533e14ff2885c965823cc20f4e751580
Morgado <morgadodesarrollador@gmail.com>
2021-05-04 13:42:21 +0200

    Editores en Api

--- Libreria/apiLibreria/src/Controllers/EditoresController.php
+++ Libreria/apiLibreria/src/Controllers/EditoresController.php
@@ -2,17 +2,11 @@
     namespace App\Controllers;
     use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Http\Message\ServerRequestInterface as Request;
-    use App\Model\CategoriasModel;    
+    use App\Model\EditoresModel;    
 
     class EditoresController {
     
-        public function new(Request  $request, Response $response, $args){
-            $response->getBody()->write("Insertar una nueva Categoria");
-            return $response
-                ->withHeader('Content-Type', 'application/json')
-                ->withStatus(200);
-        }
-       
+              
         public function getAll($request, $response, $args){
             $editores = EditoresModel::getAll();
             $editoresjson = json_encode($editores);
--- Libreria/apiLibreria/src/Routes/editores.php
+++ Libreria/apiLibreria/src/Routes/editores.php
@@ -4,6 +4,5 @@ use Slim\Routing\RouteCollectorProxy;
 
 //contendrá los entrypoints (acciones CRUD) de la tabla categorias
 $app->group('/api', function(RouteCollectorProxy $group){
-    $group->get('/categorias', 'App\Controllers\CategoriasController:getAll');
-    $group->post('/categorias/new', 'App\Controllers\CategoriasController:new');   
+    $group->get('/editores', 'App\Controllers\EditoresController:getAll');
 });
